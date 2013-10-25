1deafa312085968bb9548f04496f653acff95d8b
Tobias Harrer <tobiasharrer@gmail.com>
2013-10-25 13:59:37 +0200

    params

--- GoBi/src/blatt1/Parser.java
+++ GoBi/src/blatt1/Parser.java
@@ -54,15 +54,15 @@ public class Parser {
         if (args.length < 4 || args.length / 2 == 1 || !args[0].equals("-pairs") || !args[2].equals("-seqlib")) {// # of params needs to be even
             throw new ParamException("less than 4/wrong params");
         } else {
-            if (args[1].matches(".+\\.(in|out)?pairs")) {//"(/[a-zA-Z_0-9.]+)+"
+            if (args[1].matches("((/[a-zA-Z_0-9.]+)+)?\\.(in|out)?pairs")) {
                 params.put("-pairs", args[1]);
             } else {
-                throw new ParamException("provide a valid .pairs file");//path to the 
+                throw new ParamException("provide a valid (.pairs file or path"); 
             }
-            if (args[3].matches(".+\\.seqlib")) {//(/[a-zA-Z_0-9.]+)+
+            if (args[3].matches("((/[a-zA-Z_0-9.]+)+)?\\.seqlib")) {
                 params.put("-seqlib", args[3]);
             } else {
-                throw new ParamException("provide a valid .seqlib file");//path to the 
+                throw new ParamException("provide a valid .seqlib file or path"); 
             }
             for (int i = 4; i < args.length; i++) {//store parameters
                 switch (args[i]) {
