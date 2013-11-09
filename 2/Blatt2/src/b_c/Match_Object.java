86d233e8eea0f6a3c7fdd2f6927a0c5c8c7f6c4b
Tobias Harrer <tobiasharrer@gmail.com>
2013-11-10 00:59:25 +0100

    c)

---
+++ 2/Blatt2/src/b_c/Match_Object.java
@@ -0,0 +1,26 @@
+package b_c;
+
+/**
+ *
+ * @author tobias
+ */
+public class Match_Object {
+    
+    private final String protein_ID;
+    private final String source_DB;
+    private final float e_value;
+    private final int score;
+    private final int round;
+    
+    public Match_Object(String protein_ID, String source_DB, float e_value, int score, int round){
+        this.protein_ID = protein_ID;
+        this.source_DB = source_DB;
+        this.e_value = e_value;
+        this.score = score;
+        this.round = round;
+    }
+    
+    public Object[] getInfo(){
+        return new Object[] {protein_ID, source_DB, e_value, round};
+    }
+}
