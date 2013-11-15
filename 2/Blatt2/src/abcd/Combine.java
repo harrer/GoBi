e997c02f83699b05aa10081817da0fb7eefc13c4
harrer <tobiasharrer@gmail.com>
2013-11-15 10:31:58 +0100

    10:30

--- 2/Blatt2/src/abcd/Combine.java
+++ 2/Blatt2/src/abcd/Combine.java
@@ -1,9 +1,11 @@
 package abcd;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -11,26 +13,42 @@ import java.util.HashMap;
  */
 public class Combine {
     
+    private final String path_NR = "/home/proj/biosoft/PROTEINS/NR/";
+    private final String path_BLAST = "/home/proj/biosoft/PROTEINS/PDB_REP_CHAINS/BLAST/";
+    
     public ArrayList d() throws IOException{
-        ArrayList<Integer> gi_list = new Tax_to_GI().readFile_HashMap("/home/proj/biosoft/PROTEINS/NR/gi_taxid_prot.dmp").get(9606);
+        ArrayList<Integer> gi_list = new Tax_to_GI().readFile_HashMap(path_NR+"gi_taxid_prot.dmp").get(9606);
         HashMap<Integer, Boolean> gi_map = new HashMap();
         for (Integer gi : gi_list) {
             gi_map.put(gi, true);
         }
-        ArrayList<NR_Object> NR_list = new NR_BLAST_processor().read_NR_File("/home/proj/biosoft/PROTEINS/NR/nrdump.fasta", gi_map);
-        ArrayList<Match_Object> BLAST_list = new NR_BLAST_processor().read_BLAST_file("/home/proj/biosoft/PROTEINS/PDB_REP_CHAINS/BLAST/7cat.A.blast");
+        ArrayList<NR_Object> NR_list = new NR_BLAST_processor().read_NR_File(path_NR+"nrdump.fasta", gi_map);
         Object[] n,b;
         ArrayList<Object[]> result = new ArrayList<>();
-        for (NR_Object nr : NR_list) {
-            n = nr.getVars();
+        ArrayList<Match_Object> BLAST_list = new ArrayList();
+        String[] files = new File(path_BLAST).list();
+        long start = new Date().getTime();
+        System.out.println("starting find pdb:");
+        for (int i=0; i< 1000; i++) {//String blast_file : files
+            //if()
+            try {
+                BLAST_list = new NR_BLAST_processor().read_BLAST_file(path_BLAST + files[i]);
+            } catch (NumberFormatException numberFormatException) {
+                System.out.println(files[i]);
+            }
             for (Match_Object match : BLAST_list) {
                 b = match.getInfo();
-                if(n[0].equals(b[1]) && n[1].equals(b[2])){
-                    System.out.println("hit");
-                    result.add(b);
+                if (b[1].toString().equalsIgnoreCase("pdb")) {
+                    for (NR_Object nr : NR_list) {
+                        n = nr.getVars();
+                        if (n[1].equals(b[0])) {
+                            result.add(b);
+                        }
+                    }
                 }
             }
         }
+        System.out.println("find all pdb: "+ (new Date().getTime() - start) +" ms");
         return result;
     }
     
@@ -38,6 +56,6 @@ public class Combine {
         System.out.println("combine:");
         long start = new Date().getTime();
         ArrayList list = new Combine().d();
-        System.out.println(new Date().getTime() - start + "ms");
+        System.out.println("total "+ (new Date().getTime() - start) + "ms");
     }
 }
--- 2/Blatt2/src/abcd/Test.java
+++ 2/Blatt2/src/abcd/Test.java
@@ -1,15 +1,9 @@
 package abcd;
 
-import abcd.NR_Object;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -17,76 +11,10 @@ import java.util.regex.Pattern;
  */
 public class Test {
     
-    public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("([^>]+)");
-        String in = ">gi|66818355| >gi|sdad| asd< sda >gi|ffw|f2f ";
-        Matcher matcher = pattern.matcher(in);
-        while(matcher.find()){
-            System.out.println(matcher.group(0));
-        }
-    }
-    
-    private Object[] read_NR_File(String file) throws FileNotFoundException, IOException {   //############ b) ##########
-        FileReader fr = new FileReader(file);
+    public static void main(String[] args) throws FileNotFoundException {
+        FileReader fr = new FileReader("/home/proj/biosoft/PROTEINS/PDB_REP_CHAINS/BLAST");
         BufferedReader br = new BufferedReader(fr);
-        ArrayList<String> sequences = new ArrayList<>();
-        HashMap<String, NR_Object> map = new HashMap();
-        String line;
-        StringBuilder sb = new StringBuilder();
-        int index = 0, lines = 56096686, c = 0, h = 0;
-        long tStart = new Date().getTime();
-        Pattern p1 = Pattern.compile("gi\\|(\\d+)\\|(\\w+)\\|(.*)\\|?.*"), p2 = Pattern.compile("([^>]+)");
-        Matcher m1, m2;
-        String[] split ,sa;
-        while ((line = br.readLine()) != null) {
-            if (100.0 * c / lines >= h) {//time measuring
-                System.out.println(h + "% " + "completed");
-                h++;
-            }
-            c++;
-            if (line.startsWith(">")) {
-                //sequences.add(sb.toString());
-                //sb.delete(0, sb.length());
-                index++;
-                m2 = p2.matcher(line);
-                while (m2.find()) {
-                    m1 = p1.matcher(m2.group(0));
-                    m1.find();
-                    try{
-                        //map.put(m1.group(1), new NR_Object("", m1.group(3), m1.group(2), index));
-                    }catch(IllegalStateException e){
-                        
-                    }
-                }
-            } //else {
-                //sb.append(line);
-            //}
-        }
-        long runTime = new Date().getTime() - tStart;
-        System.out.println("finished readFile in " + ((runTime / 60000) % 60) + " min " + ((runTime / 1000) % 60) + "s; " + runTime + " ms");
-        return new Object[]{map, sequences};
-    }
-    
-    public static void heapInfo(){
-        int mb = 1024*1024;
-         
-        //Getting the runtime reference from system
-        Runtime runtime = Runtime.getRuntime();
-         
-        System.out.println("##### Heap utilization statistics [MB] #####");
-         
-        //Print used memory
-        System.out.println("Used Memory:"
-            + (runtime.totalMemory() - runtime.freeMemory()) / mb);
- 
-        //Print free memory
-        System.out.println("Free Memory:"
-            + runtime.freeMemory() / mb);
-         
-        //Print total available memory
-        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
- 
-        //Print Maximum available memory
-        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+        String 
+        while()
     }
 }
