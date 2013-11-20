e12dee38677a8aef53f17da4a3fcaaa0fed327ca
harrer <tobiasharrer@gmail.com>
2013-11-20 17:33:17 +0100

    a) testing

--- Blatt3/src/a/Main.java
+++ Blatt3/src/a/Main.java
@@ -23,30 +23,28 @@ public class Main {
         BufferedReader br = new BufferedReader(fr);
         String line, gene_id = "", transcript_id = "", exon_number = "";
         String[] split, split_id, id;
+        Gene g = null;
+        Transcript transcript = null;
         while ((line = br.readLine()) != null) {//read lines
             split = line.split("\t");
             if (split[2].equals("CDS")) {//coding sequence
                 split_id = split[8].split(";");
                 id = split_id[0].split("\"");//get gene_id
-                Gene g = null;
                 if (!id[1].equals(gene_id)) {//new gene
                     g = new Gene(id[1], split[0], split[6]);
-                    gene.put(id[1], g);
+                    //gene.put(id[1], g);
                     gene_id = id[1];
-                }
-                else{
+                } else {
                     id = split_id[1].split("\"");
-                    Transcript transcript = null;
-                    if(!id[1].equals(transcript_id)){
+                    if (!id[1].equals(transcript_id)) {//new Transcript
                         transcript_id = id[1];
-                        transcript = new Transcript(new Protein(new Exon(split_id[2].split("\"")[1], Long.parseLong(split[4]), Long.parseLong(split[5])), split_id[5].split("\"")[1]));
+                        transcript = new Transcript(new Protein(new Exon(split_id[2].split("\"")[1], Long.parseLong(split[3]), Long.parseLong(split[4])), split_id[5].split("\"")[1]));
+                    } else {
+                        transcript.getProtein().addExon(new Exon(split_id[2].split("\"")[1], Long.parseLong(split[3]), Long.parseLong(split[4])));
                     }
-                    else{
-                        transcript.getProtein().addExon(new Exon(split_id[2].split("\"")[1], Long.parseLong(split[4]), Long.parseLong(split[5])));
-                    }
-                    g.addTranscript(transcript_id, transcript);
-                    gene.put(transcript_id, g);
                 }
+                g.addTranscript(transcript_id, transcript);
+                gene.put(transcript_id, g);
             }
         }
         br.close();
@@ -61,7 +59,9 @@ public class Main {
         return gene.get(geneId);
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        String file = "/home/proj/biosoft/GENOMIC/HUMAN/Homo_sapiens.GRCh37.63.gtf";
+        m.parse_file(file);
     }
 }
