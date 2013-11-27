2d9ad28617e4a18092d200172f7adc9d50610ca4
harrer <tobiasharrer@gmail.com>
2013-11-27 12:02:27 +0100

    b) fertig

--- Blatt3/src/b/GenomeSequenceExtractor.java
+++ Blatt3/src/b/GenomeSequenceExtractor.java
@@ -1,6 +1,8 @@
 package b;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -10,17 +12,26 @@ import java.io.RandomAccessFile;
  */
 public class GenomeSequenceExtractor {
     
-    public String readExon(long start, long stop, String file) throws FileNotFoundException, IOException{
+    private final String path = "/home/proj/biosoft/GENOMIC/HUMAN/HUMAN_GENOME_FASTA/Homo_sapiens.GRCh37.63.dna.chromosome.";
+    
+    public String readExon(long start, long stop, String chromosome) throws FileNotFoundException, IOException{
+        String file = path+chromosome+".fa";
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line = br.readLine();//open filehandle to check length of header line
+        int header_offset = line.length() + 1;
+        start += (header_offset + (start/60));
+        stop += (header_offset + (stop/60));//skip the newline/linebreaks
+        br.close(); fr.close();
         RandomAccessFile raf = new RandomAccessFile(file, "r");
-        byte[] b = new byte[(int) (stop-start)];
-        raf.seek(start);
+//        RandomAccessFile raf = new RandomAccessFile(chromosome, "r");
+        byte[] b = new byte[(int) (stop-start+1)];
+        raf.seek(start-1);
         raf.read(b);
         return new String(b);
     }
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String file = "/home/proj/biosoft/GENOMIC/HUMAN/HUMAN_GENOME_FASTA/Homo_sapiens.GRCh37.63.dna.chromosome.Y.fa";
-        String s = new GenomeSequenceExtractor().readExon(2, 5, "/tmp/test1347");
-        System.out.println(s);
+        System.out.println(new GenomeSequenceExtractor().readExon(59930, 60200, "X"));
     }
 }
