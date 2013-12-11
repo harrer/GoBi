8acea2c57cb796c874b7ad2685594b51b3589c5b
harrer <tobiasharrer@gmail.com>
2013-12-11 13:45:03 +0100

    Blatt4

--- Blatt3/src/bc/GenomicUtils.java
+++ Blatt3/src/bc/GenomicUtils.java
@@ -103,7 +103,7 @@ public class GenomicUtils {
         String[] split;
         while ((line = br.readLine()) != null) {
             split = line.split(":");
-
+            
             if (split.length == 2) {
                 map.put(split[0], split[1]);
             }
