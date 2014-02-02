b06131b5f3312ea47a851e1c235c29d62e8c2c46
Tobias Harrer <tobiasharrer@gmail.com>
2014-02-02 18:31:32 +0100

    6-1

--- Blatt4/src/blatt1/Test.java
+++ Blatt4/src/blatt1/Test.java
@@ -1,8 +1,11 @@
 package blatt1;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -11,7 +14,17 @@ import java.io.InputStreamReader;
 public class Test {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("exec /home/proj/biosoft/PROTEINS/software/TMalign ../PDB_REP_CHAINS/STRUCTURES/1bj4.A.pdb ../PDB_REP_CHAINS/STRUCTURES/2dkj.A.pdb").getInputStream())).readLine());
+//        System.out.println(new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("exec /home/proj/biosoft/PROTEINS/software/TMalign ../PDB_REP_CHAINS/STRUCTURES/1bj4.A.pdb ../PDB_REP_CHAINS/STRUCTURES/2dkj.A.pdb").getInputStream())).readLine());
+        BufferedReader br = new BufferedReader(new FileReader("/home/tobias/Desktop/mart_export.txt"));
+        String line;
+        ArrayList<String> all = new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+            all.add(line.split("\\s")[1]);
+        }
+        HashMap<String, Boolean> map = new HashMap<>();
+        for (String string : all) {
+            map.put(string, Boolean.TRUE);
+        }
+        System.out.println("");
     }
-
 }
