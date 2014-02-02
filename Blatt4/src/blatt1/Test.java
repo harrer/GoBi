ff7b238c1afce925c9591ec133847c426145a186
harrer <tobiasharrer@gmail.com>
2014-02-02 04:45:49 +0100

    5abc

--- Blatt4/src/blatt1/Test.java
+++ Blatt4/src/blatt1/Test.java
@@ -1,13 +1,17 @@
 package blatt1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author harrert
  */
 public class Test {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        System.out.println(new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("exec /home/proj/biosoft/PROTEINS/software/TMalign ../PDB_REP_CHAINS/STRUCTURES/1bj4.A.pdb ../PDB_REP_CHAINS/STRUCTURES/2dkj.A.pdb").getInputStream())).readLine());
     }
 
 }
